f7e620e00c4e8b2dab2ee84058595eb3ff6cb5f3
Ursula Eponon <ulaeponon@gmail.com>
2025-06-02 17:25:55 +0200

    add debug corrigé

--- syntaxe/debug.js
+++ syntaxe/debug.js
@@ -1,16 +1,19 @@
- Nous souhaitons faire la somme du prix de plusieurs articles
-// Prix de deux articles
-let a1 = 10
+// Nous souhaitons faire la somme du prix de plusieurs articles
+let a1 = 10 
+// ceci represente le prix de l'article 1
 let a2 = 20
+// ceci represente le prix de l'article 2
 // Nous calculons le total
-const tot = a1 + a2
+   tot= a1+a2
+   console.log("le prix total des articles a1 et a2 est de:",tot)
 // Prix d'un troisieme article
-let a3 = "50"
+let a3 = 50
 // Nous ajoutons a3 au total
 tot = tot + a3
-
+console.log("le total du prix des articles a1, a2 et a3 est de:",tot)
 console.log("Le prix total avant reduction est de : ", tot)
-console.log("Le prix total après reduction est de : ", tot2)
-
 //Nous appliquons 20% de reduction
-let tot2 = tot - tot*0,2
+let tot2 = tot - tot*0.2
+let reduction = tot*0.2 // Ceci represente la reduction
+tot2 = tot - reduction
+console.log("le prix apres avoir appliqué la reduction est de:",tot2)
