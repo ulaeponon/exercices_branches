7c68f3726a068ba6a706b86a9e7dfa344548d3de
Ursula Eponon <ulaeponon@gmail.com>
2025-06-02 14:42:10 +0200

    add debug exercices

---
+++ syntaxe/debug.js
@@ -0,0 +1,16 @@
+ Nous souhaitons faire la somme du prix de plusieurs articles
+// Prix de deux articles
+let a1 = 10
+let a2 = 20
+// Nous calculons le total
+const tot = a1 + a2
+// Prix d'un troisieme article
+let a3 = "50"
+// Nous ajoutons a3 au total
+tot = tot + a3
+
+console.log("Le prix total avant reduction est de : ", tot)
+console.log("Le prix total après reduction est de : ", tot2)
+
+//Nous appliquons 20% de reduction
+let tot2 = tot - tot*0,2
